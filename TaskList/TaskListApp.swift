3ad961c5973ef21046c3f7bc72309d74c5b0bbda
Josh <josh.ruth@gmail.com>
2022-12-18 15:59:12 -0800

    Initial Commit

---
+++ TaskList/TaskListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskListApp.swift
+//  TaskList
+//
+//  Created by Josh Rutheiser on 12/18/22.
+//
+
+import SwiftUI
+
+@main
+struct TaskListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
